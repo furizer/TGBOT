d309afe0ef04e8086b56cce6fabbf7f3d87d2d11
furizer <furizeroff@gmail.com>
2024-09-20 16:37:31 +0700

    init project

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log(12222);
+import { configDotenv } from "dotenv"
+import { Bot } from "grammy"
+configDotenv()
+const bot = new Bot(process.env.API_TOKEN)
+
+
+bot.api.setMyCommands(
+    [
+        {
+            command: 'start', description: 'Запуск Бота'
+        },
+        {
+            command: 'hello', description: 'Сказать'
+        },
+    ]
+)
+
+bot.command('start', async (ctx) => {
+    await ctx.reply('Я бот')
+})
+
+bot.command('hello', async (ctx) => {
+    await ctx.reply('Привет')
+})
+
+bot.catch(err => {
+    console.log(err)
+})
+
+
+bot.start()
